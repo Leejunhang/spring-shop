63082f9caadf333e0bfaec4dfe530fce19a81970
Leejunhang <share97@naver.com>
2023-11-23 18:20:40 +0900

    first commit

---
+++ src/components/FooterPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterPage = () => {
+    return (
+        <div className='my-5'>
+            <h5 className='text-center'>CopyRight 2023.Lee all rights reserved.</h5>
+        </div>
+    )
+}
+
+export default FooterPage
