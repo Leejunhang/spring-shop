c576590138a637825cb359b9e532fc6e8d8a2c9f
Leejunhang <share97@naver.com>
2023-11-24 18:20:29 +0900

    ver02

--- src/components/NaviPage.jsx
+++ src/components/NaviPage.jsx
@@ -3,12 +3,23 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Routes, useLocation, Route } from 'react-router-dom';
 import SearchPage from './shop/SearchPage';
+import ShopList from './shop/ShopList';
+import ShopUpdate from './shop/ShopUpdate';
+import LoginPage from './user/LoginPage';
 
 const NaviPage = () => {
     const location = useLocation();
     const path=location.pathname;
+	const onLogout = (e) => {
+		e.preventDefault();
+		if(window.confirm("로그아웃 하실래요?")){
+			sessionStorage.clear();
+			window.location.href="/";
+		}
+	}
+
     return (
-        <>
+    <>
         <Navbar expand="lg" bg="primary" data-bs-theme="dark">
           <Container fluid>
             <Navbar.Brand href="/">Logo</Navbar.Brand>
@@ -19,16 +30,34 @@ const NaviPage = () => {
                 style={{ maxHeight: '100%' }}
                 navbarScroll>
                 <Nav.Link href="/shop/search" className={path.indexOf('/shop/search')!==-1 && 'active'}>상품검색</Nav.Link>
-                <Nav.Link href="/shop/list" className={path.indexOf('/shop/list')!==-1 && 'active'}>상품목록</Nav.Link>
+                <Nav.Link href="/shop/list" className={path.indexOf('/shop/')!==-1 && 'active'}>상품관리</Nav.Link>
               </Nav>
               <Nav>
-                    <Nav.Link href="/login" className={path.indexOf('/login')!==-1 && 'active'}>로그인</Nav.Link>
+					{sessionStorage.getItem("uid") ?
+						<>	
+							<Nav.Link href="/mypage" className='active'>
+								{sessionStorage.getItem("uid")}
+							</Nav.Link>
+							<Nav.Link href="/logout" onClick={onLogout}>
+								로그아웃
+							</Nav.Link>
+						</>
+						:
+						<Nav.Link href="/login" className={path.indexOf('/login')!==-1 && 'active'}>
+							로그인
+						</Nav.Link>
+					}
+                
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
+
         <Routes>
             <Route path="/shop/search" element={<SearchPage/>}/>
+            <Route path="/shop/list" element={<ShopList/>}/>
+			<Route path="/shop/update/:pid" element={<ShopUpdate/>}/>
+			<Route path="/login" element={<LoginPage/>}/>
         </Routes>
     </>
     )
--- src/components/shop/SearchPage.jsx
+++ src/components/shop/SearchPage.jsx
@@ -1,21 +1,27 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Table, InputGroup, Form, Row, Col, Button } from 'react-bootstrap';
+import { Table, InputGroup, Form, Row, Col, Button, Spinner } from 'react-bootstrap';
 
 const SearchPage = () => {
+    const [loading, setLoading] = useState(false);
     const [list, setList] = useState([]);
     const [query, setQuery] = useState("맥북");
     const [page, setPage] = useState(1);
+    const [cnt, setCnt] = useState(0);
+    
     const getList = async() => {
+        setLoading(true);
         const res=await axios(`/search/list.json?page=${page}&size=5&query=${query}`);
         //console.log(res.data);
-        const data=res.data.items;
+        let data=res.data.items.map(s=>s && {...s, title:stripHtmlTags(s.title)});
+        data=data.map(item=>item && {...item, checked: false});
         setList(data);
+        setLoading(false);
     }
 
     useEffect(()=>{
         getList();
-    }, []);
+    }, [page]);
 
     const onSubmit = (e)=>{
         e.preventDefault();
@@ -33,6 +39,49 @@ const SearchPage = () => {
         }
     }
 
+      // HTML 태그를 제거하는 함수
+    const stripHtmlTags = (htmlString) => {
+        const doc = new DOMParser().parseFromString(htmlString, 'text/html');
+    return doc.body.textContent || "";
+    }
+
+    const onChangeAll = (e) => {
+        const data=list.map(item=>item && {...item, checked:e.target.checked});
+        setList(data);
+    }
+
+    const onChangeSingle = (e, pid) => {
+        const data=list.map(item=> item.productId === pid ? {...item, checked:e.target.checked} : item);
+        setList(data);
+    }
+    
+    useEffect(()=>{
+        let chk=0;
+        list.forEach(item=>{
+            if(item.checked) chk++;
+        });
+        setCnt(chk);
+    }, [list]);
+
+    const onCheckedSave = async() => {
+        if(cnt==0) {
+            alert("저장할 상품을 선택하세요!");
+        }else{
+            //선택 저장
+            if(window.confirm(`${cnt}개 상품을 등록하실래요?`)){
+                for(const item of list){
+                    if(item.checked){
+                        await axios.post("/shop/insert", item);
+                    }
+                }
+                alert("상품등록 완료!");
+                getList();
+            }
+        }
+    }
+
+    if(loading) return
+        <div className='my-5 text-center'><Spinner variant='primary'/></div>
     return (
         <div className='my-5'>
             <h1 className='text-center mb-5'>상품검색</h1>
@@ -46,10 +95,14 @@ const SearchPage = () => {
                         </InputGroup>
                     </form>
                 </Col>
+                <Col className='text-end'>
+                    <Button onClick={onCheckedSave}>선택저장</Button>
+                </Col>
             </Row>
             <Table striped bordered hover>
                 <thead>
                     <tr>
+                        <td><input type="checkbox" onChange={onChangeAll} checked={list.length===cnt}/></td>
                         <td>ID</td><td>이미지</td><td>제목</td>
                         <td>가격</td><td>제조사</td><td>상품등록</td>
                     </tr>
@@ -57,6 +110,8 @@ const SearchPage = () => {
                 <tbody>
                     {list.map(s=>
                     <tr key={s.productId}>
+                        <td><input onChange={(e)=>onChangeSingle(e, s.productId)} 
+                            type="checkbox" checked={s.checked}/></td>
                         <td>{s.productId}</td>
                         <td><img src={s.image} width="50"/></td>
                         <td><div className='ellipsis'>{s.title}</div></td>
